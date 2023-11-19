bcb0c76c442c08c691fd0a6c247719b57699128e
skhammari <skhammari@gmail.com>
2023-11-19 23:34:57 +0330

    Implement Guardian API

---
+++ app/Console/Services/GuardianService.php
@@ -0,0 +1,39 @@
+<?php
+
+	namespace App\Console\Services;
+
+	class GuardianService implements NewsUpdaterInterface
+	{
+
+		const URL = 'https://content.guardianapis.com/search';
+		public function sendRequest($url, ?array $params)
+		{
+			$apikey = getenv('GUARDIAN_API_KEY');
+			$params = array_merge($params, [
+				'api-key' => $apikey
+			]);
+			$response = \Http::get($url, $params);
+
+			return $response;
+		}
+
+		public function update()
+		{
+			$response = $this->sendRequest(self::URL, []);
+			$articles = $response->json()['response']['results'];
+			$news = [];
+			foreach ($articles as $article) {
+				$news[] = [
+					'title'       => $article['webTitle'],
+					'url'         => $article['webUrl'],
+					'sourceName'  => 'The Guardian',
+					'sourceId'    => $article['id'],
+					'author'      => '',
+					'category'    => $article['sectionName'],
+					'publishedAt' => $article['webPublicationDate'],
+				];
+			}
+
+			return $news;
+		}
+	}
